bea7a3ea447f94a4d2e7ee988c5dc76fdf9c41aa
sreethi <sreejas7919@gmail.com>
2026-06-02 20:55:53 +0530

    Add files via upload

---
+++ Dsa2.py
@@ -0,0 +1,33 @@
+# Python Data Types Example
+
+# Integer
+age = 20
+
+# Float
+height = 5.6
+
+# String
+name = "Sree"
+
+# Boolean
+is_student = True
+
+# List
+colors = ["Red", "Blue", "Green"]
+
+# Tuple
+numbers = (1, 2, 3)
+
+# Dictionary
+student = {
+    "name": "Sree",
+    "age": 20
+}
+
+print("Integer:", age)
+print("Float:", height)
+print("String:", name)
+print("Boolean:", is_student)
+print("List:", colors)
+print("Tuple:", numbers)
+print("Dictionary:", student)
