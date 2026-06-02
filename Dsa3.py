d35d41557070695c957dbfeaa7e05b8751e714a1
sreethi <sreejas7919@gmail.com>
2026-06-02 20:58:19 +0530

    Add files via upload

---
+++ Dsa3.py
@@ -0,0 +1,23 @@
+# Python Operators Example
+
+a = 10
+b = 5
+
+# Arithmetic Operators
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+# Comparison Operators
+print("a > b:", a > b)
+print("a < b:", a < b)
+print("a == b:", a == b)
+
+# Logical Operators
+print("a > 5 and b < 10:", a > 5 and b < 10)
+print("a > 5 or b > 10:", a > 5 or b > 10)
+
+# Assignment Operator
+c = a + b
+print("Value of c:", c)
