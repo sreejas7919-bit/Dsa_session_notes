6add9a4a407dc5899ac273d53438c7456e061fb2
sreethi <sreejas7919@gmail.com>
2026-06-02 21:03:49 +0530

    Add files via upload

---
+++ Dsa4.py
@@ -0,0 +1,27 @@
+# Python Conditional Statements
+
+age = 18
+
+# if statement
+if age >= 18:
+    print("You are eligible to vote")
+
+# if-else statement
+number = 7
+
+if number % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+# if-elif-else statement
+mark = 85
+
+if mark >= 90:
+    print("Grade A+")
+elif mark >= 75:
+    print("Grade A")
+elif mark >= 50:
+    print("Grade B")
+else:
+    print("Fail")
